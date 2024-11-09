482e038872dd2a4720fcff863301114083e65be8
ErklO1 <vefremov1233@gmail.com>
2024-11-10 00:01:50 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string=input('Мою собаку зовут: ')
+character_count= len(my_string)
+print("Количество символов:", character_count)
+print(my_string.upper())
+print(my_string.lower())
+print('Она любит грызть косточки)'.replace(' ','#'))
+last_character='Она любит грызть косточки)'[-1]
+print(last_character)
